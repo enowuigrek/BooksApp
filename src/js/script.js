1eda6fad2dbe8f52d022f93cf6c25d47e695953e
Łukasz Nowak <enowuigrek@gmail.com>
2022-03-30 21:59:43 +0200

    exercise 3 - 9.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,58 +1,67 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
-'use strict';
 
-    const select = {
-        books: {
-            booksList: '.books-list',
-            bookImage: '.book__image',
-            dataId: 'data-id'
-        },
-        templateOf: {
-            templateBook: '#template-book',
-        },
-    };
+  'use strict';
 
-    const templates = {
-        templateBook: Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML),
-    };
+  const select = {
+    books: {
+      booksList: '.books-list',
+      bookImage: '.book__image',
+      dataId: 'data-id'
+    },
 
-    function render(){
-        const allBooks = dataSource.books;
+    templateOf: {
+      templateBook: '#template-book',
+    },
+  };
 
-        for(let book of allBooks){
-            const generatedHTML = templates.templateBook(book);
-            const generatedDOM = utils.createDOMFromHTML(generatedHTML);
-            const booksList = document.querySelector(select.books.booksList);
+  const templates = {
+    templateBook: Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML),
+  };
 
-            booksList.appendChild(generatedDOM);
-        }
-    };
+  function render(){
+    const allBooks = dataSource.books;
 
-    const favoriteBooks = [];
+    for(let book of allBooks){
+      const generatedHTML = templates.templateBook(book);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      const booksList = document.querySelector(select.books.booksList);
 
-    function initActions(){
-        
-        const booksList = document.querySelectorAll(select.books.bookImage);
-
-        for(const clickedBook of booksList) {
-            
-            clickedBook.addEventListener('dblclick', function(event){
-                event.preventDefault();
-
-                const dataId = clickedBook.getAttribute(select.books.dataId);
-
-                clickedBook.classList.add('favorite');
-                
-                favoriteBooks.push(dataId);
-
-            });
-        }
-        
+      booksList.appendChild(generatedDOM);
     }
+  }
 
-    render();
-    initActions();
+  const favoriteBooks = [];
+  
 
+  function initActions(){
+    const booksList = document.querySelectorAll(select.books.bookImage);
+
+    for(const clickedBook of booksList){
+            
+      clickedBook.addEventListener('dblclick', function(event){
+        event.preventDefault();
+
+        const dataId = clickedBook.getAttribute(select.books.dataId);
+
+        if (clickedBook.classList.contains('favorite')){
+          const bookIndex = favoriteBooks.indexOf(dataId);
+
+          clickedBook.classList.remove('favorite');
+
+          favoriteBooks.splice(bookIndex,1);
+          
+        } else {
+          clickedBook.classList.add('favorite');
+
+          favoriteBooks.push(dataId);
+        }
+        console.log(favoriteBooks);
+      });
+    }
+  }
+
+  render();
+  initActions();
 }
