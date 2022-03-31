343e2728136c6a4ea4d8d67e4c3a6383b39f63a4
Łukasz Nowak <enowuigrek@gmail.com>
2022-03-31 13:34:29 +0200

    exercise 6 - 9.2

--- src/js/script.js
+++ src/js/script.js
@@ -20,11 +20,8 @@
   const templates = {
     templateBook: Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML),
   };
-  console.log(templates);
 
-
-  
-
+  const allBooks = dataSource.books;
   const favoriteBooks = [];
   const filters = [];
 
@@ -39,14 +36,42 @@
   }
 
   function render(){
-    const allBooks = dataSource.books;
+
+    const thisBook = this;
 
     for(let book of allBooks){
+
+      const ratingBgc = thisBook.determineRatingBgc(book.rating);
+      book.ratingBgc = ratingBgc;
+
+      const ratingWidth = book.rating * 10;
+      book.ratingWidth = ratingWidth;
+
       const generatedHTML = templates.templateBook(book);
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
       const booksList = document.querySelector(select.books.booksList);
 
       booksList.appendChild(generatedDOM);
+    }
+  }
+
+  function filterBooks(){
+    for(let book of allBooks){
+      let shouldBeHidden = false;
+      const filterBook = document.querySelector('.book__image[data-id="' + book.id + '"]');
+
+      for(let filter of filters){
+        if(!book.details[filter]){
+          shouldBeHidden = true;
+          break;
+        }
+      }
+      
+      if(!shouldBeHidden){
+        filterBook.classList.remove('hidden');
+      } else{
+        filterBook.classList.add('hidden');
+      }
     }
   }
 
@@ -84,11 +109,26 @@
           filters.splice(filterIndex ,1);
         }
       }
-      
-      console.log(filters);
+      filterBooks();
     });
   }
 
+  function determineRatingBgc(rating){
+
+    let ratingStyle = '';
+
+    if(rating < 6){
+      ratingStyle = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    }else if(rating > 6 && rating <= 8){
+      ratingStyle = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    }else if(rating > 8 && rating <= 9){
+      ratingStyle = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    }else if(rating > 9){
+      ratingStyle = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+    return ratingStyle;
+  }
+//   determineRatingBgc();
   render();
   getElemets();
   initActions();
